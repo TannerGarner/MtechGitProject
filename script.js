7fcb61d18f2f1850423d2ac029c8a4e9f6b1d29c
TannerMtecStudent <tanner.garner8843@stu.mtec.edu>
2024-08-21 19:20:40 -0600

    Added correct javascript to make the caluculator function.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $('.button').on('click', function(){
     let content = $(this).html();
     $('#display').append(content);
-}
+})
+
+$('.clear').on('click', function(){
+    $('#display').empty()
+})
+
+$('.equals').on('click', function(){
+    let compute = $('#display').text()
+    compute = eval(compute)
+    $('#display').text(compute)
+})
